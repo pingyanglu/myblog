34bb3f2e6dd7393a5138599779efae4e4f23bb0e
pingyanglu <871820453@qq.com>
2020-01-14 08:52:39 +0800

    'deploy'

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,8 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:pingyanglu/myblog.git master:gh-pages
+
+git add .
+git commit -m 'deploy'
+git push -u -f origin master
